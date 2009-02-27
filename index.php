c426fdaad5312b92a9cc64611cfb09f03796e9b8
Benjamin Birkenhake <benjamin@birkenhake.org>
2009-02-27 19:32:34 +0100

    Added first Lines and (mostly) Documentation

---
+++ index.php
@@ -0,0 +1,15 @@
+<?
+
+/**
+ * "Heimweh" is a Minimalistic XML Web Content Management System
+ * 
+ * I have to say thank you to all the great Developers who inspired this project by their passion.
+ * Especially those who are Part of Drupal.org Wordpress.org Zeit.de and Digitalkombinat.net
+ * 
+ * 
+ * @author Benjamin Birkenhake <benjamin@birkenhake.org>
+ * 
+ */
+
+
+print "Hallo Heimweh!\n\n";
